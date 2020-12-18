945bc3106f036d39b841605fcbe38e16816ce246
Derek <derekkiminho@gmail.com>
2020-12-18 13:47:13 -0800

    Functional webcam component that will render a preview and successfully host image onto s3 with file name. and save in database

--- react-app/src/components/camera/Camera.js
+++ react-app/src/components/camera/Camera.js
@@ -1,8 +1,10 @@
 import React, {useState, useRef, useCallback, useEffect} from "react";
+import { Redirect } from 'react-router-dom';
 import Webcam from "react-webcam";
 import {useSelector} from 'react-redux'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
+import {uploadImage, createPost, getPosts} from '../../services/post'
 
 const videoConstraints = {
     width: 600,
@@ -11,14 +13,46 @@ const videoConstraints = {
 };
 
 const WebcamComponent = () => {
-    const currentuser = useSelector(state => state.users.user)
+    const currentUser = useSelector(state => state.users.user)
     const webcamRef = useRef(null)
     const [imgSrc, setImgSrc] = useState(null)
-    const [file, setFile] = useState({})
+    const [image, setImage] = useState({})
+    const [fileName, setFileName] = useState("")
     const [caption, setCaption] = useState("")
+    const [redirect, setRedirect] = useState(false)
+    const [posts, setPosts] = useState(0)
+    const [test, setTest] = useState("testing now")
+
+    useEffect(() => {
+        (async () => {
+            const allPosts = await getPosts(currentUser.id)
+            setPosts(allPosts.posts.length)
+            // console.log(allPosts.posts.length)
+            // setTest("Working now")
+            // debugger
+        })()
+    },[currentUser.id])
+
+    useEffect(() => {
+        (async () => {
+            if(imgSrc && posts){
+                // await setPosts(posts + 1)
+                debugger
+                setFileName(currentUser.fullname + posts.toString())
+                debugger
+            }
+
+        })()
+    },[imgSrc])
+
+    useEffect(() => {
+        if(fileName){
+            setImage(dataURLtoFile(imgSrc, fileName))
+        }
+    }, [fileName])
 
     // This file takes the base64 encoded data from the webcam, and converts it to a file to send to S3.
-    function dataURLtoFile(dataurl, filename) {
+    const dataURLtoFile = (dataurl, filename) => {
 
         var arr = dataurl.split(','),
             mime = arr[0].match(/:(.*?);/)[1],
@@ -33,29 +67,43 @@ const WebcamComponent = () => {
         return new File([u8arr], filename, {type:mime});
     }
 
+    const updateValue = (setfunc) => (e) => {
+        setfunc(e.target.value)
+    }
+
     const capture = useCallback(
         () => {
             setImgSrc(webcamRef.current.getScreenshot())
-        },
-        [webcamRef]
-    )
+            // console.log("No posts", posts)
+            // console.log(test)
+            // setImagePreview(webcamRef.current.getScreenshot())
+        },[webcamRef])
 
-    useEffect(() => {
-        if(imgSrc ){
-            setCaption(imgSrc.slice(30, 50) + imgSrc.slice(60, 80))
-            const filz = dataURLtoFile(imgSrc, "myfilename")
-            console.log(filz)
+    const submitImagePost = async (e) => {
+        e.preventDefault();
+        const data = new FormData();
+        // debugger
+        data.append("file", image)
+        console.log(image)
 
+        const newImage = await uploadImage(data)
+        const imageUrl = newImage.output
+
+        const post = await createPost( caption, imageUrl);
+        if(!post.errors){
+            setRedirect(true)
+
+        } else {
+            console.log('something went wrong', image)
         }
-    },[imgSrc])
+    }
 
-    // if(imgSrc){
-    //     // console.log(imgSrc)
-    //     // setCaption(imgSrc.slice(0, 20))
-    // }
+    if(redirect){
+        return <Redirect to={`/`} />
+    }
 
-    // if(caption){
-    //     console.log(caption)
+    // if(posts !== 0){
+    //     console.log("posts here, ", posts)
     // }
 
     return (
@@ -73,7 +121,7 @@ const WebcamComponent = () => {
                 <button onClick={capture}>Capture Photo</button>
             </div>
             <div>
-                <form encType='multipart/formdata'>
+                <form encType='multipart/formdata' onSubmit={submitImagePost}>
                     <TextField
                             name='caption'
                             type='text'
@@ -81,12 +129,20 @@ const WebcamComponent = () => {
                             variant="outlined"
                             placeholder='Tell us about your image...'
                             value={caption}
+                            onChange={updateValue(setCaption)}
                             required
                             fullWidth
                             />
-                    {/* <TextField fullWidth variant="outlined" type="file" name="user_file" required onChange={setImageHelper}/>
-                    <Button className="edit_button" color="primary" variant="contained" type="submit">Post</Button> */}
+                    {/* <TextField fullWidth variant="outlined" type="file" name="user_file" required onChange={setImageHelper}/> */}
+                    <Button className="edit_button" color="primary" variant="contained" type="submit">Post</Button>
                 </form>
+            </div>
+            <div>
+            {imgSrc && (
+                <img
+                src={imgSrc}
+                />
+            )}
             </div>
 
         </div>
