385851472b6326a4898e2edbebfcaa4fb95f1be7
Derek <derekkiminho@gmail.com>
2020-12-21 11:43:04 -0800

    Redid NavLinks to properly be routed to the icons

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -4,30 +4,30 @@ import LogoutButton from './auth/LogoutButton';
 import IconButton from '@material-ui/core/IconButton';
 import ExploreIcon from '@material-ui/icons/Explore';
 import AddBoxIcon from '@material-ui/icons/AddBox';
+import HomeIcon from '@material-ui/icons/Home';
 
 const NavBar = ({ setAuthenticated }) => {
   return (
     <nav className="nav_container">
       <div className="navbar_container">
         <div className="navbar_homepage_text">
-          <NavLink className="navbar_text" to="/" exact={true} activeClassName="active">
+          <NavLink className="navbar_text" to="/" exact={true} activeClassName="active_logo">
             Pinstagram
           </NavLink>
         </div>
         <div className="navbar_button_container">
           <div>
-          <NavLink to="/post/create" exact={true} activeClassName="active">
-              <IconButton>
-                <AddBoxIcon/>
+            <NavLink to ="/" exact={true} activeClassName="active"/>
+          </div>
+          <div>
+              <IconButton component={NavLink} to="/post/create">
+                <AddBoxIcon />
               </IconButton>
-            </NavLink>
           </div>
           <div className="navbar_discover">
-            <NavLink to="/discover" exact={true} activeClassName="active">
-              <IconButton>
+              <IconButton component={NavLink} to="/discover">
                 <ExploreIcon/>
               </IconButton>
-            </NavLink>
           </div>
           <div className="logout_button">
             <LogoutButton setAuthenticated={setAuthenticated} />
