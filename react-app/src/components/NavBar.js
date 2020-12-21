9308416354de17517ce076ab67aaad557f6a7574
Derek <derekkiminho@gmail.com>
2020-12-21 12:01:19 -0800

    Added active classes to navbar icons to better represent which page a user is currently on

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -5,8 +5,21 @@ import IconButton from '@material-ui/core/IconButton';
 import ExploreIcon from '@material-ui/icons/Explore';
 import AddBoxIcon from '@material-ui/icons/AddBox';
 import HomeIcon from '@material-ui/icons/Home';
+import {makeStyles} from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+    icon: {
+      '&.active': {
+        color: "black"
+      },
+      color: "gray"
+    }
+  }
+)
 
 const NavBar = ({ setAuthenticated }) => {
+  const classes = useStyles()
+
   return (
     <nav className="nav_container">
       <div className="navbar_container">
@@ -17,16 +30,19 @@ const NavBar = ({ setAuthenticated }) => {
         </div>
         <div className="navbar_button_container">
           <div>
-            <NavLink to ="/" exact={true} activeClassName="active"/>
+            {/* <NavLink to ="/" exact={true} activeClassName="active"/> */}
+            <IconButton className={classes.icon} component={NavLink} to ="/" activeClassName="active" exact={true}>
+              <HomeIcon />
+            </IconButton>
           </div>
           <div>
-              <IconButton component={NavLink} to="/post/create">
+              <IconButton className={classes.icon} component={NavLink} to="/post/create" activeClassName="active" exact={true}>
                 <AddBoxIcon />
               </IconButton>
           </div>
           <div className="navbar_discover">
-              <IconButton component={NavLink} to="/discover">
-                <ExploreIcon/>
+              <IconButton className={classes.icon} component={NavLink} to="/discover" activeClassName="active" exact={true}>
+                <ExploreIcon />
               </IconButton>
           </div>
           <div className="logout_button">
